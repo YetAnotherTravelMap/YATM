e92e373db6de943487f5a54f346a111bf68450c9
Tuna Uygun <uyguntuna2018@gmail.com>
2024-11-02 12:39:42 -0400

    Initialize Backend Project

---
+++ backend/YATM/src/main/java/com/yetanothertravelmap/yatm/YatmApplication.java
@@ -0,0 +1,13 @@
+package com.yetanothertravelmap.yatm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YatmApplication.class, args);
+    }
+
+}
