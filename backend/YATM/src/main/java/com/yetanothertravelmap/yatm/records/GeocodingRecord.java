77903b6737c61438d4833e23d5a6df0a9567544a
Tuna Uygun <uyguntuna2018@gmail.com>
2024-11-06 23:02:04 -0500

    Set zoom level based on bounding box

--- backend/YATM/src/main/java/com/yetanothertravelmap/yatm/records/GeocodingRecord.java
+++ backend/YATM/src/main/java/com/yetanothertravelmap/yatm/records/GeocodingRecord.java
@@ -2,6 +2,8 @@ package com.yetanothertravelmap.yatm.records;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record GeocodingRecord(int place_id, float lat, float lon, String name, String display_name) {
+public record GeocodingRecord(int place_id, float lat, float lon, String name, String display_name, List<String> boundingbox) {
 }
--- frontend/src/components/SearchBox/SearchResultsListItem.jsx
+++ frontend/src/components/SearchBox/SearchResultsListItem.jsx
@@ -1,11 +1,16 @@
 import './SearchResultsListItem.css'
 import {useMap} from "react-leaflet";
+import L from "leaflet";
 
 function SearchResultsListItem({result, resetSearchResults}) {
     const map = useMap()
 
     function handleClick() {
-        map.flyTo([result.lat, result.lon], 15);
+        const box = L.latLngBounds(
+            [parseFloat(result.boundingbox[0]), parseFloat(result.boundingbox[2])], // Southwest corner
+            [parseFloat(result.boundingbox[1]), parseFloat(result.boundingbox[3])]  // Northeast corner
+        );
+        map.flyToBounds(box, {maxzoom: 16});
         resetSearchResults();
     }
 
