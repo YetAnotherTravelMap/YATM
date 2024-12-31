8296ff7faa70f1fb76bcaf2b1cc6533b13c6f0bc
Tuna Uygun <uyguntuna2018@gmail.com>
2024-12-31 16:23:41 -0500

    Create a map for the user after registration

--- backend/YATM/src/main/java/com/yetanothertravelmap/yatm/service/RegistrationService.java
+++ backend/YATM/src/main/java/com/yetanothertravelmap/yatm/service/RegistrationService.java
@@ -1,6 +1,8 @@
 package com.yetanothertravelmap.yatm.service;
 
+import com.yetanothertravelmap.yatm.model.Map;
 import com.yetanothertravelmap.yatm.model.User;
+import com.yetanothertravelmap.yatm.repository.MapRepository;
 import com.yetanothertravelmap.yatm.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,15 +12,20 @@ public class RegistrationService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    private final MapRepository mapRepository;
 
 
-    public RegistrationService(UserRepository userRepository){
+    public RegistrationService(UserRepository userRepository, MapRepository mapRepository){
         this.userRepository = userRepository;
+        this.mapRepository = mapRepository;
     }
 
     public User saveUser(User user){
         user.setHash(encoder.encode(user.getHash()));
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+        Map userMap = new Map("Travel Map", savedUser);
+        mapRepository.save(userMap);
+        return savedUser;
     }
 
     public User getUserByUsername(String username){
