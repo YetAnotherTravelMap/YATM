df34c0dff93435e79d700f0a612bcaa41ffb2ecf
Tuna Uygun <uyguntuna2018@gmail.com>
2025-03-26 00:21:35 -0400

    Update CorsConfig.java

--- backend/YATM/src/main/java/com/yetanothertravelmap/yatm/config/CorsConfig.java
+++ backend/YATM/src/main/java/com/yetanothertravelmap/yatm/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl)
+                .allowedOrigins("https://yatm.pages.dev") // Your frontend domain
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
